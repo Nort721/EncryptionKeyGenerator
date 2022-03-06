edb062d197230aaf38480b1fd3bcb9ab5fc365da
Nort721 <itag7894@gmail.com>
2022-03-07 00:17:27 +0200

    more modern GUI look & made main class final

--- src/com/nort721/gen/Main.java
+++ src/com/nort721/gen/Main.java
@@ -1,8 +1,15 @@
 package com.nort721.gen;
 
-public class Main {
+import javax.swing.*;
+
+public final class Main {
 
     public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         EncryptionKeyGenerator encryptionKeyGenerator = new EncryptionKeyGenerator();
         encryptionKeyGenerator.start();
     }
